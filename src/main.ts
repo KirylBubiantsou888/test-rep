4f6b8dff1d73bf0eae88b9d80f04af7cc6f045e8
Kirill Bubentsov <kirill.bubentsov@splitmetrics.com>
2025-01-18 23:25:41 +0300

    wip

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+// import { ExpressAdapter } from '@nestjs/platform-express';
+// import * as express from 'express';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   await app.listen(process.env.PORT ?? 3000);
+// }
+// bootstrap();
+
+
 import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
+import * as express from 'express';
+
+const expressApp = express();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
+  app.enableCors(); // Enable CORS if needed
+  await app.init();
 }
 bootstrap();
+
+export const handler = (req: any, res: any) => {
+  return expressApp(req, res);
+};
